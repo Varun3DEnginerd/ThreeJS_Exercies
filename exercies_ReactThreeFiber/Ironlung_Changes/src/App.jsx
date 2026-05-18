ef42b8dbf1971c5ef95ad25070413662113d4ecf
varung28 <varung.gajre87@gmail.com>
2026-05-18 13:19:24 +0530

    Water_Changes_02
    -backup for waterChange in 'Ironlung_Changes'
    -backup for React Application in 'react-Without_VITE'
    -backup for Vite addition which is ongoing in 'react-With_Vite_WIP'
    
    TODO:
    -port my OpenGL+Windows Water into this without Gerstner Equation and then add NormalMap

---
+++ exercies_ReactThreeFiber/Ironlung_Changes/src/App.jsx
@@ -0,0 +1,33 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Lighting } from "./Lighting";
+import { ReflectiveBloodWater } from "./ReflectiveBloodWater";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="canvas-full">
+      <Canvas camera={{ position: [6, 6, 6], fov: 50 }}>
+        <color attach="background" args={["#0a0c12"]} />
+        <Lighting />
+        {/* <mesh position={[0, 0.5, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial
+            color="#7c9cff"
+            roughness={0.3}
+            metalness={0.2}
+            emissive="#7c9cff"
+            emissiveIntensity={0.3}
+          />
+        </mesh> */}
+        <Suspense fallback={null}>
+          <ReflectiveBloodWater />
+        </Suspense>
+        <OrbitControls enableDamping />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
