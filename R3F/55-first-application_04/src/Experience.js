128c870ef5d5f75edaef9b87be90f59214e4e6d1
varung28 <varung.gajre87@gmail.com>
2026-07-21 13:11:54 +0530

    Arrow Vertices with Head and Tail
    -updated new vertices for both
    -updated rotate,scale and position for both
    -It is now ready to be used with application

--- R3F/55-first-application_04/src/Experience.js
+++ R3F/55-first-application_04/src/Experience.js
@@ -6,27 +6,27 @@ import { useMemo } from 'react';
 
 export default function Experience() {
 
-    const newGeometry = useMemo(() => {
+    const ArrowTail = useMemo(() => {
         const shape1 = new THREE.BufferGeometry();
 
-        const vertices = new Float32Array([
+        const tail_Vertices = new Float32Array([
             // top : 0,1,2,3
             1.0, 1.0, -1.0,
             -1.0, 1.0, -1.0,
-            -1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.25,
+            1.0, 1.0, 1.25,
 
             // bottom: 4,5,6,7
             1.0, -1.0, -1.0,
             -1.0, -1.0, -1.0,
-            -1.0, -1.0, 1.0,
-            1.0, -1.0, 1.0,
+            -1.0, -1.0, 1.25,
+            1.0, -1.0, 1.25,
 
             // front: 8,9,10,11
-            1.0, 1.0, 1.0,
-            -1.0, 1.0, 1.0,
-            -1.0, -1.0, 1.0,
-            1.0, -1.0, 1.0,
+            1.0, 1.0, 1.25,
+            -1.0, 1.0, 1.25,
+            -1.0, -1.0, 1.25,
+            1.0, -1.0, 1.25,
 
             // back: 12,13,14,15
             1.0, 1.0, -1.0,
@@ -36,15 +36,15 @@ export default function Experience() {
 
             // right: 16,17,18,19
             1.0, 1.0, -1.0,
-            1.0, 1.0, 1.0,
-            1.0, -1.0, 1.0,
+            1.0, 1.0, 1.25,
+            1.0, -1.0, 1.25,
             1.0, -1.0, -1.0,
 
             // left: 20,21,22,23
-            -1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.25,
             -1.0, 1.0, -1.0,
             -1.0, -1.0, -1.0,
-            -1.0, -1.0, 1.0
+            -1.0, -1.0, 1.25
         ]);
 
         const indices = new Uint16Array([
@@ -74,7 +74,7 @@ export default function Experience() {
 
         ]);
 
-        const colors = new Float32Array([
+        const tail_Colors = new Float32Array([
             //color-red      
             1.0, 0.0, 0.0,
             1.0, 0.0, 0.0,
@@ -112,8 +112,8 @@ export default function Experience() {
             1.0, 1.0, 0.0,
         ]);
 
-        shape1.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
-        shape1.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+        shape1.setAttribute("position", new THREE.BufferAttribute(tail_Vertices, 3));
+        shape1.setAttribute("color", new THREE.BufferAttribute(tail_Colors, 3));
 
         // shape1.setIndex(indices);
         shape1.setIndex(Array.from(indices));
@@ -125,13 +125,77 @@ export default function Experience() {
     }, []);
 
 
+    const ArrowHead = useMemo(() => {
+        const shape2 = new THREE.BufferGeometry();
+
+        const arrowHead_Vertices = new Float32Array([
+            // front
+            0.0, 1.0, 0.0,
+            -1.0, -1.0, 0.75,
+            1.0, -1.0, 0.75,
+
+            // right
+            0.0, 1.0, 0.0,
+            1.0, -1.0, 0.75,
+            1.0, -1.0, -0.75,
+
+            // back
+            0.0, 1.0, 0.0,
+            1.0, -1.0, -0.75,
+            -1.0, -1.0, -0.75,
+
+            // left
+            0.0, 1.0, 0.0,
+            -1.0, -1.0, -0.75,
+            -1.0, -1.0, 0.75,
+        ]);
+
+        const arrowHead_Colors = new Float32Array([
+             1.0,0.0,0.0,
+            0.0,1.0,0.0,
+            0.0,0.0,1.0,
+
+            1.0,0.0,0.0,
+            0.0,0.0,1.0,
+            0.0,1.0,0.0,
+
+            1.0,0.0,0.0,
+            0.0,1.0,0.0,
+            0.0,0.0,1.0,
+
+            1.0,0.0,0.0,
+            0.0,0.0,1.0,
+            0.0,1.0,0.0,
+        ]);
+
+        shape2.setAttribute("position", new THREE.BufferAttribute(arrowHead_Vertices, 3));
+        shape2.setAttribute("color", new THREE.BufferAttribute(arrowHead_Colors, 3));
+
+        // shape2.setIndex(indices);
+        //shape2.setIndex(Array.from(indices));
+
+        shape2.computeVertexNormals();
+
+
+        return (shape2);
+    }, []);
+
+
     return (
-        <mesh geometry={newGeometry} scale={[1.25, 0.5, 1]}>
+        <>
+        <mesh geometry={ArrowTail} scale={[1.25, 0.5, 1]}>
             <meshBasicMaterial
                 vertexColors
                 side={THREE.DoubleSide}
-            //wireframe
             />
         </mesh>
+        
+        <mesh geometry={ArrowHead} scale={[1.5,1, 1]} position={[0, 0, -1.75]} rotation={[-Math.PI/2, 0, 0]} >
+            <meshBasicMaterial
+                vertexColors
+                side={THREE.DoubleSide}
+            />
+        </mesh>
+        </>
     );
 }
